abb05bf24dbd6232415c499ff309335a956e9f83
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-03-05 17:56:42 -0800

    Initial Commit

---
+++ Maui10Contacts/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui10Contacts;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Maui10Contacts/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Maui10Contacts;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
